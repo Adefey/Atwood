53de57389f94d934d022d86c11e0caf66b64ce15
Андрей Коленков <avoknelok@mail.ru>
2020-11-13 22:03:37 +0300

    Создал класс для физики

---
+++ Atwood/Physics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atwood
+{
+    class Physics
+    {
+        String Sashik;
+
+        public Physics()
+        {
+            Sashik = "Raznoos)";
+        }
+    }
+}
