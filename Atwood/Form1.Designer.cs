f4550e77f4e3ebae95ad4849288b471da3e60e81
Андрей Коленков <avoknelok@mail.ru>
2020-11-24 00:47:28 +0300

    Разделил классы по файлам. Период обновления таймера увеличен до 50

--- Atwood/Form1.Designer.cs
+++ Atwood/Form1.Designer.cs
@@ -270,7 +270,7 @@ namespace Atwood
             // 
             // timer1
             // 
-            this.timer1.Interval = 20;
+            this.timer1.Interval = 50;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // vScrollBar1
---
+++ Atwood/Weights.cs
@@ -0,0 +1,10 @@
+﻿namespace Atwood
+{
+    internal static class Weights
+    {
+        public const double BaseWeight = 0.06,
+                      AddWeight1 = 0.0065,
+                      AddWeight2 = 0.0085,
+                      AddWeight3 = 0.012;
+    }
+}
